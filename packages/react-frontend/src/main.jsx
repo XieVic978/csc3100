826f37b1cd28a15835dd5e29035398d285e7a19a
Victor <Xievic978@gmail.com>
2026-08-28 15:59:36 -0700

    starting E1

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import "./main.css"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function MyApp(){
+  return (
+    <div>Hello, React!</div>
+  );
+}
+
+//Create the container
+const container = document.getElementByID("root");
+
+//Create Root
+const root = ReactDOMClient.createRoot(container);
+
+//Initial render: Render an element to the Root
+root.render(<MyApp />)
