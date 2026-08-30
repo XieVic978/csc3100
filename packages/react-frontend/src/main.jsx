f4b46a10c1665b9ba12af8e1b498798be67d2c86
Victor <Xievic978@gmail.com>
2026-08-29 20:30:17 -0700

    added prop,state, delete

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import ReactDOMClient from "react-dom/client";
-import "./main.css"
+import MyApp from "./MyApp"
+import "./main.css";
 
-function MyApp(){
-  return (
-    <div>Hello, React!</div>
-  );
-}
+
 
 //Create the container
-const container = document.getElementByID("root");
+const container = document.getElementById("root");
 
 //Create Root
 const root = ReactDOMClient.createRoot(container);
 
 //Initial render: Render an element to the Root
 root.render(<MyApp />)
+
